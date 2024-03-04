f63a7dcc7007bf07eb7995980e64b3563d3923ec
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-03-04 16:10:09 +0700

    Add project files.

---
+++ 041_Cobditionalstartment/041_Cobditionalstartment.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float Matematika, Fisika, rerata;
+	string status;
+
+	cout << "Masukkan Nilai Matematika : ";
+	cin >> Matematika;
+	cout << "Masukkan Nilai Fisika : ";
+	cin >> Fisika;
+
+	rerata = (Matematika + Fisika) / 2;
+
+	if (rerata >= 60 || Matematika > 70) {
+		status = "Lulus";
+	}
+	else {
+		status = "Tidak lulus";
+	}
+
+	cout << "Status Kelulusan : " << status << endl;
+
+	return 0;
+}
